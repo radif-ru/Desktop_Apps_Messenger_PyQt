e9a0cddd48a044dee993c418248c5c3390ae13b8
Radif <mail@radif.ru>
2021-11-15 18:38:28 +0300

    refactoring

---
+++ lessons/lesson03/client.py
@@ -0,0 +1,244 @@
+"""Программа-клиент"""
+
+import argparse
+import sys
+import json
+import threading
+from socket import socket, AF_INET, SOCK_STREAM
+import time
+from threading import Thread
+
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT, MESSAGE, SENDER, MESSAGE_TEXT, EXIT, DESTINATION
+from common.utils import get_message, send_message
+from decos import log
+from errors import ReqFieldMissingError, ServerError, IncorrectDataReceivedError
+
+from logs.configs.config_client_log import LOGGER as CLIENT_LOGGER  # Инициализация клиентского логера
+from metaclasses import ClientMaker
+
+
+# Класс формировки и отправки сообщений на сервер и взаимодействия с пользователем.
+class ClientSender(Thread, metaclass=ClientMaker):
+    def __init__(self, account_name, sock):
+        self.account_name = account_name
+        self.sock = sock
+        super().__init__()
+
+    def create_exit_message(self):
+        """Функция создаёт словарь с сообщением о выходе"""
+        return {
+            ACTION: EXIT,
+            TIME: time.time(),
+            ACCOUNT_NAME: self.account_name
+        }
+
+    def create_message(self):
+        """
+        Функция запрашивает кому отправить сообщение и само сообщение,
+        и отправляет полученные данные на сервер
+        :param sock:
+        :param account_name:
+        :return:
+        """
+        to_user = input('Введите получателя сообщения: ')
+        message = input('Введите сообщение для отправки: ')
+        message_dict = {
+            ACTION: MESSAGE,
+            SENDER: self.account_name,
+            DESTINATION: to_user,
+            TIME: time.time(),
+            MESSAGE_TEXT: message
+        }
+        CLIENT_LOGGER.debug(f'Сформирован словарь сообщения: {message_dict}')
+        try:
+            send_message(self.sock, message_dict)
+            CLIENT_LOGGER.info(f'Отправлено сообщение для пользователя {to_user}')
+        except:
+            CLIENT_LOGGER.critical('Потеряно соединение с сервером.')
+            sys.exit(1)
+
+    def run(self):
+        """Функция взаимодействия с пользователем, запрашивает команды, отправляет сообщения"""
+        self.print_help()
+        while True:
+            command = input('Введите команду: ')
+            if command == 'message':
+                self.create_message()
+            elif command == 'help':
+                self.print_help()
+            elif command == 'exit':
+                try:
+                    send_message(self.sock, self.create_exit_message())
+                except:
+                    pass
+                print('Завершение соединения.')
+                CLIENT_LOGGER.info('Завершение работы по команде пользователя.')
+                # Задержка неоходима, чтобы успело уйти сообщение о выходе
+                time.sleep(0.5)
+                break
+            else:
+                print('Команда не распознана, попробойте снова. help - вывести поддерживаемые команды.')
+
+    def print_help(self):
+        """Функция выводящяя справку по использованию"""
+        print('Поддерживаемые команды:')
+        print('message - отправить сообщение. Кому и текст будет запрошены отдельно.')
+        print('help - вывести подсказки по командам')
+        print('exit - выход из программы')
+
+
+# Класс-приёмник сообщений с сервера. Принимает сообщения, выводит в консоль.
+class ClientReader(threading.Thread, metaclass=ClientMaker):
+    def __init__(self, account_name, sock):
+        self.account_name = account_name
+        self.sock = sock
+        super().__init__()
+
+    # Основной цикл приёмника сообщений, принимает сообщения, выводит в консоль. Завершается при потере соединения.
+    def run(self):
+        """Функция - обработчик сообщений других пользователей, поступающих с сервера"""
+        while True:
+            try:
+                message = get_message(self.sock)
+                if ACTION in message and message[ACTION] == MESSAGE and \
+                        SENDER in message and DESTINATION in message \
+                        and MESSAGE_TEXT in message and message[DESTINATION] == self.account_name:
+                    print(f'\nПолучено сообщение от пользователя {message[SENDER]}:'
+                          f'\n{message[MESSAGE_TEXT]}')
+                    CLIENT_LOGGER.info(f'Получено сообщение от пользователя {message[SENDER]}:'
+                                       f'\n{message[MESSAGE_TEXT]}')
+                else:
+                    CLIENT_LOGGER.error(f'Получено некорректное сообщение с сервера: {message}')
+            except IncorrectDataReceivedError:
+                CLIENT_LOGGER.error(f'Не удалось декодировать полученное сообщение.')
+            except (OSError, ConnectionError, ConnectionAbortedError,
+                    ConnectionResetError, json.JSONDecodeError):
+                CLIENT_LOGGER.critical(f'Потеряно соединение с сервером.')
+                break
+
+
+@log
+def create_new_presence(account_name='Guest'):
+    """
+    Функция генерирует запрос о присутствии клиента
+    :param account_name: Guest - имя аккаунта по умолчанию
+    :return: out:  возвращает сгенерированный словарь словарь
+    """
+    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
+    return out
+
+
+@log
+def process_response_answer(message):
+    """
+    Функция разбирает ответ сервера на сообщение о присутствии,
+    возращает 200 если все ОК или генерирует исключение при ошибке
+    :param message: проверяется код ответа от сервера
+    :return:
+    """
+    CLIENT_LOGGER.debug(f'Разбор сообщения от сервера: {message}')
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        elif message[RESPONSE] == 400:
+            raise ServerError(f'400 : {message[ERROR]}')
+    raise ReqFieldMissingError(RESPONSE)
+
+
+@log
+def arg_parser():
+    """Создаём парсер аргументов коммандной строки и читаем параметры, возвращаем 3 параметра"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-n', '--name', default=None, nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+    client_name = namespace.name
+
+    # проверим подходящий номер порта
+    if not 1023 < server_port < 65536:
+        CLIENT_LOGGER.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}. '
+            f'Допустимы адреса с 1024 до 65535. Клиент завершается.')
+        exit(1)
+
+    return server_address, server_port, client_name
+
+
+def main():
+    """Сообщаем о запуске"""
+
+    # Загружаем параметы коммандной строки
+    # client.py 192.168.1.2 8079 ...
+    server_address, server_port, client_name = arg_parser()
+
+    # Если имя пользователя не было задано, необходимо запросить пользователя.
+    if not client_name:
+        client_name = input('Введите имя пользователя: ')
+
+    """Сообщаем о запуске"""
+    print(f'Консольный месседжер. Клиентский модуль. Имя пользователя: {client_name}')
+
+    CLIENT_LOGGER.info(
+        f'Запущен клиент с парамертами: адрес сервера: {server_address}, '
+        f'порт: {server_port}, имя пользователя: {client_name}')
+
+    # Инициализация сокета и сообщение серверу о нашем появлении
+    try:
+        client_socket = socket(AF_INET, SOCK_STREAM)
+        client_socket.connect((server_address, server_port))
+        send_message(client_socket, create_new_presence(client_name))
+        answer = process_response_answer(get_message(client_socket))
+        CLIENT_LOGGER.info(f'Установлено соединение с сервером. Ответ сервера: {answer}')
+        print(f'Установлено соединение с сервером.')
+    except json.JSONDecodeError:
+        CLIENT_LOGGER.error('Не удалось декодировать полученную Json строку.')
+        sys.exit(1)
+    except ServerError as error:
+        CLIENT_LOGGER.error(f'При установке соединения сервер вернул ошибку: {error.text}')
+        sys.exit(1)
+    except ReqFieldMissingError as missing_error:
+        CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле {missing_error.missing_field}')
+        sys.exit(1)
+    except (ConnectionRefusedError, ConnectionError):
+        CLIENT_LOGGER.critical(
+            f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+            f'конечный компьютер отверг запрос на подключение.')
+        sys.exit(1)
+    else:
+        # Если соединение с сервером установлено корректно,
+        # запускаем клиенский процесс приёма сообщений
+        module_receiver = ClientReader(client_name, client_socket)
+        module_receiver.daemon = True
+        module_receiver.start()
+
+        # затем запускаем отправку сообщений и взаимодействие с пользователем.
+        module_sender = ClientSender(client_name, client_socket)
+        module_sender.daemon = True
+        module_sender.start()
+        CLIENT_LOGGER.debug('Запущены процессы')
+
+        # Watchdog основной цикл, если один из потоков завершён,
+        # то значит или потеряно соединение или пользователь
+        # ввёл exit. Поскольку все события обработываются в потоках,
+        # достаточно просто завершить цикл.
+        while True:
+            time.sleep(1)
+            if module_receiver.is_alive() and module_sender.is_alive():
+                continue
+            break
+
+
+if __name__ == '__main__':
+    main()
---
+++ lessons/lesson07/doc/build/html/_sources/__sphinx-apidoc_rst_files/modules.rst.txt
@@ -0,0 +1,17 @@
+lesson07
+========
+
+.. toctree::
+   :maxdepth: 4
+
+   client
+   client
+   common
+   launcher
+   launcher_gui
+   logs
+   server
+   server
+   server_database
+   server_gui
+   unit_tests
